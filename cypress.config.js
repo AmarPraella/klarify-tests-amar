9aab609c39b838c60e0df473e10ca2f3f994d7a1
FaisalShaikhHA <faisal@hulkapps.com>
2023-09-18 11:58:16 +0530

    updates file structure & adds new assertions

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        numTestsKeptInMemory: 1,
+        experimentalMemoryManagement: true,
     },
-  },
 });
